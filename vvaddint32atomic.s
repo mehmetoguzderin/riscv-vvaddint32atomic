3f4ea5c9fc78befef7d94001f139940da6a31b99
Mehmet Oguz Derin <mehmetoguzderin@mehmetoguzderin.com>
2022-06-20 19:05:42 +0300

    First commit

--- vvaddint32atomic.s
+++ vvaddint32atomic.s
@@ -1,23 +1,23 @@
 .global vvaddint32atomic
 .type vvaddint32atomic, @function
 vvaddint32atomic:
-    fence   rw, rw
-    lw      t0, 0(a4)
-    fence   r, rw
-    bge     t0, a0, .vvaddint32atomic_return
-    sub     t1, a0, t0
-    vsetvli t2, t1, e32, m1, ta, ma
-    amoadd.w.aqrl   t0, t2, (a4)
-    bge     t0, a0, .vvaddint32atomic_return
-    add     t2, t2, t0
-    slli    t0, t0, 2
-    add     t3, a1, t0
-    add     t4, a2, t0
-    add     t5, a3, t0
-    vle32.v v0, (t3)
-    vle32.v v1, (t4)
-    vadd.vv v2, v0, v1
-    vse32.v v2, (t5)
-    blt     t2, a0, vvaddint32atomic
+fence rw, rw
+lw t0, (a4)
+fence r, rw
+bge t0, a0, .vvaddint32atomic_return
+sub t1, a0, t0
+vsetvli t2, t1, e32, m1, ta, ma
+amoadd.w.aqrl t0, t2, (a4)
+bge t0, a0, .vvaddint32atomic_return
+add t2, t2, t0
+slli t0, t0, 2
+add t3, a1, t0
+add t4, a2, t0
+add t5, a3, t0
+vle32.v v0, (t3)
+vle32.v v1, (t4)
+vadd.vv v2, v0, v1
+vse32.v v2, (t5)
+blt t2, a0, vvaddint32atomic
 .vvaddint32atomic_return:
-    ret
+ret
